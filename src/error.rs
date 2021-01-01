fb4bcf306f5cceeb371710226a2e89edf84fe2df
Sean Kelley <seansfkelley@gmail.com>
2021-01-01 10:53:51 -0500

    Tweak error handling.

--- src/error.rs
+++ src/error.rs
@@ -4,14 +4,14 @@ use std::fmt;
 #[derive(Debug)]
 pub enum PasspromptError {
   NoPasswordsDefined,
-  ImproperSaltLength(usize, usize),
+  IllegalByteArrayLength { expected: usize, actual: usize },
   UnparseableWaitFormat(String),
 }
 
 impl fmt::Display for PasspromptError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
-      Self::ImproperSaltLength(expected, actual) => {
+      Self::IllegalByteArrayLength { expected, actual } => {
         write!(f, "improper length: expected {}, got {}", expected, actual)
       }
       Self::NoPasswordsDefined => {
--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,11 @@
-use std::error;
-
 use crate::error::PasspromptError;
 
-pub fn byte_vec_to_array<const LEN: usize>(s: Vec<u8>) -> Result<[u8; LEN], Box<dyn error::Error>> {
+pub fn byte_vec_to_array<const LEN: usize>(s: Vec<u8>) -> Result<[u8; LEN], PasspromptError> {
   if s.len() != LEN {
-    return Err(Box::new(PasspromptError::ImproperSaltLength(LEN, s.len())));
+    return Err(PasspromptError::IllegalByteArrayLength {
+      expected: LEN,
+      actual: s.len(),
+    });
   }
   let mut bytes_array = [0; LEN];
   for (i, b) in s.iter().enumerate() {
