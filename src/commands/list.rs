9c5ee87e0daf5af922769ea1be7fe866c7bfa44f
Sean Kelley <seansfkelley@gmail.com>
2021-01-10 11:05:36 -0500

    Sort output of list command.

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,9 +1,14 @@
+use std::iter::FromIterator;
+
 use crate::commands::CommandResult;
 use crate::config;
 
 pub fn command(config: &config::Config) -> Result<CommandResult, Box<dyn std::error::Error>> {
-  for (name, _) in config.passwords.iter() {
-    println!("{}", name);
+  let mut names = Vec::from_iter(config.passwords.keys());
+  names.sort_unstable();
+
+  for n in names {
+    println!("{}", n);
   }
 
   Ok(CommandResult {
